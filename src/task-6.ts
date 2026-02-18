16c2887abcf3f82a05c201efde0579a9721ecf48
Sofia <sofiakucmida@gmail.com>
2026-02-18 13:35:43 +0200

    01-ts-basics

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,9 +2,9 @@ function getFirstElement<T>(arr: T[]): T {
         return arr[0];
 }
 
-getFirstElement([1, 2, 3]);           // 1
-getFirstElement(["a", "b", "c"]);     // "a"
-getFirstElement([true, false, true]); // true
+getFirstElement<number>([1, 2, 3]);           // 1
+getFirstElement<string>(["a", "b", "c"]);     // "a"
+getFirstElement<boolean>([true, false, true]); // true
 
 // console.log(getFirstElement([1, 2, 3]));
 // console.log(getFirstElement(["a", "b", "c"]));
